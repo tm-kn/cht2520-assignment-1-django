1c0265e0b7422ced9621c8c65ed54a3fcadf6342
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2018-10-16 14:38:52 +0100

    Add sheets and projects app models
    Closes #1

--- timetracker/settings/base.py
+++ timetracker/settings/base.py
@@ -26,6 +26,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sitemaps',
+
+    'timetracker.projects',
+    'timetracker.sheets',
 ]
 
 
