fa2d55cd9fa9c5406fe6fd98aafe3585065fe44a
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2019-01-17 22:03:54 +0000

    Rename datepicker component

---
+++ timetracker/static/js/components/datePicker.js
@@ -0,0 +1,12 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+export default () => {
+    flatpickr('.js-datetime-picker', {
+        enableSeconds: true,
+        enableTime: true,
+    });
+
+
+    flatpickr('.js-date-picker');
+};
