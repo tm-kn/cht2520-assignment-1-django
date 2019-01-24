0ca40ceb9ee790f44ab8e18c1f2338a17598eba9
Tomasz Knapik <u1562595@unimail.hud.ac.uk>
2019-01-24 03:01:12 +0000

    Add navbar

---
+++ timetracker/static/js/components/navBar.js
@@ -0,0 +1,16 @@
+// Inspired from https://bulma.io/documentation/components/navbar/
+
+export default function() {
+    // Get all "navbar-burger" elements
+    for(const burger of document.querySelectorAll('.navbar-burger')) {
+        burger.addEventListener('click', () => {
+
+            const target = document.getElementById(burger.dataset.target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            burger.classList.toggle('is-active');
+            target.classList.toggle('is-active');
+
+        });
+    }
+};
--- timetracker/static/js/index.js
+++ timetracker/static/js/index.js
@@ -1,7 +1,9 @@
 import setupDatePicker from '@components/datePicker';
 import setupDeleteButton from '@components/deleteButton';
+import setupNavBar from '@components/navBar';
 
 document.addEventListener('DOMContentLoaded', () => {
     setupDatePicker();
     setupDeleteButton();
+    setupNavBar();
 });
